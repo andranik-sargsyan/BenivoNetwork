07bee385c02b6ec9dbc7de03ed6b0d9b27d0a575
Andranik <csharp.and@hotmail.com>
2021-10-25 21:40:51 +0400

    Added comments area

--- BenivoNetwork/Content/Scripts/script.js
+++ BenivoNetwork/Content/Scripts/script.js
@@ -1,1 +1,42 @@
-﻿
+﻿let people = [
+    {
+        name: "Keira Knightley",
+        age: 36,
+        isMarried: true
+    },
+    {
+        name: "Tom Cruise",
+        age: 59,
+        isMarried: true
+    },
+    {
+        name: "Al Pacino",
+        age: 81,
+        isMarried: false
+    },
+    {
+        name: "Tom Smith",
+        age: 25,
+        isMarried: false
+    },
+    {
+        name: "Poxos",
+        age: 40,
+        isMarried: true
+    },
+    {
+        name: "Andranik",
+        age: 31,
+        isMarried: true
+    },
+    {
+        name: "Lily Rose Depp",
+        age: 22,
+        isMarried: false
+    },
+    {
+        name: "Sirusho",
+        age: 34,
+        isMarried: true
+    }
+];
