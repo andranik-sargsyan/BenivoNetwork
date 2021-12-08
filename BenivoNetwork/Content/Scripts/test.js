c79e0c39d97d2d898e863c1d7d271ecfda05b0ad
Andranik <csharp.and@hotmail.com>
2021-12-08 23:12:46 +0400

    Finalized the mocks

--- BenivoNetwork/Content/Scripts/test.js
+++ BenivoNetwork/Content/Scripts/test.js
@@ -1,15 +1,3 @@
 ﻿$(() => {
-    let btn = $("#btn");
 
-    btn.click(function () {
-        arr.push({
-            name: $("#name").val(),
-            age: $("#age").val()
-        });
-
-        localStorage.setItem("people", JSON.stringify(arr));
-    });
 });
-
-let peopleTable = localStorage.getItem("people");
-let arr = peopleTable && JSON.parse(peopleTable) || [];
